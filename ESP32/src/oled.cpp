5398a92efc5040c0fd4f26eeef9cfae4b232c3ff
Filipe Cavalheiro <cavalheirofilipe@gmail.com>
2025-03-13 10:55:50 +0000

    Started working on the OLED installed some of the necessary librares

---
+++ ESP32/src/oled.cpp
@@ -0,0 +1,57 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SH1107.h>
+
+Adafruit_SH1107 display = Adafruit_SH1107(64, 128, &Wire);
+
+// OLED FeatherWing buttons map to different pins depending on board:
+#if defined(ESP8266)
+  #define BUTTON_A  0
+  #define BUTTON_B 16
+  #define BUTTON_C  2
+#elif defined(ESP32) && \
+    !defined(ARDUINO_ADAFRUIT_FEATHER_ESP32S2) && \
+    !defined(ARDUINO_ADAFRUIT_FEATHER_ESP32S3) && \
+    !defined(ARDUINO_ADAFRUIT_FEATHER_ESP32S3_NOPSRAM)
+  #define BUTTON_A 15
+  #define BUTTON_B 32
+  #define BUTTON_C 14
+#elif defined(ARDUINO_STM32_FEATHER)
+  #define BUTTON_A PA15
+  #define BUTTON_B PC7
+  #define BUTTON_C PC5
+#elif defined(TEENSYDUINO)
+  #define BUTTON_A  4
+  #define BUTTON_B  3
+  #define BUTTON_C  8
+#elif defined(ARDUINO_NRF52832_FEATHER)
+  #define BUTTON_A 31
+  #define BUTTON_B 30
+  #define BUTTON_C 27
+#else // 32u4, M0, M4, nrf52840, esp32-s2, esp32-s3 and 328p
+  #define BUTTON_A  9
+  #define BUTTON_B  6
+  #define BUTTON_C  5
+#endif
+
+// put function declarations here:
+int myFunction(int, int);
+int result;
+
+void setup() {
+  // put your setup code here, to run once:
+  result = myFunction(2, 3);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println(result);
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
+}
